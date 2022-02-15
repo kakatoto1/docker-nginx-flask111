f28768e643817283f341961b573a4d4b0a12a74b
kakatoto1 <98189795+kakatoto1@users.noreply.github.com>
2022-02-14 21:11:47 -0500

    update my name again again

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Fan Yu"
+        name = "Fan Yu Hello World"
         return render_template('index.html', name=name)
